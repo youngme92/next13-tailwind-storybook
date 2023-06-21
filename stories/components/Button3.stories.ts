14fd3b13fb83d890caf0b177c8e1911f3485be39
youngme <youngme992@gmail.com>
2023-06-21 18:26:11 +0900

    update button story

--- stories/components/Button3.stories.ts
+++ stories/components/Button3.stories.ts
@@ -1,27 +1,45 @@
+import React, { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { Button3 } from '../ui/Button3';
+import { Button3 } from '#/ui/Button3';
 
 // More on how to set up stories at: https://storybook.js.org/docs/react/writing-stories/introduction
 const meta: Meta<typeof Button3> = {
-  title: 'Example/Button3',
+  title: 'Component/Button/Button3',
   component: Button3,
   tags: ['autodocs'],
-  // argTypes: {
-  //   backgroundColor: {
-  //     control: 'color',
-  //   },
-  // },
+  argTypes: {
+    // backgroundColor: {
+    //   control: 'color',
+    // },
+    kind: {
+      control: 'select',
+    },
+    size: {
+      control: 'select',
+    },
+    disabled: {
+      control: 'boolean',
+    },
+  },
 };
 
 export default meta;
 type Story = StoryObj<typeof Button3>;
 
 // More on writing stories with args: https://storybook.js.org/docs/react/writing-stories/args
-export const Primary: Story = {
-  // args: {
-  //   primary: true,
-  //   label: 'Button',
-  // },
+export const Default: Story = {
+  args: {
+    borderless: false,
+    label: 'Button',
+    kind: 'default',
+  },
+};
+export const Error: Story = {
+  args: {
+    borderless: false,
+    label: 'Button',
+    kind: 'error',
+  },
 };
 
 // export const Secondary: Story = {
--- ui/Button3.tsx
+++ ui/Button3.tsx
@@ -1,49 +1,61 @@
+'use client';
+
 import clsx from 'clsx';
 
 interface ButtonProps {
-  // /**
-  //  * Is this the principal call to action on the page?
-  //  */
-  // primary?: boolean;
-  // /**
-  //  * What background color to use
-  //  */
-  // backgroundColor?: string;
-  // /**
-  //  * How large should the button be?
-  //  */
-  // size?: 'small' | 'medium' | 'large';
-  // /**
-  //  * Button contents
-  //  */
-  // label: string;
-  // /**
-  //  * Optional click handler
-  //  */
   /**
-   * kind
+   * borderless
    */
-  kind?: string;
+  borderless?: boolean;
+  /**
+   * button의 disabled
+   */
+  disabled?: boolean;
+  /**
+   * How large should the button be?
+   */
+  size?: 'small' | 'medium' | 'large';
+  /**
+   * Button contents
+   */
+  label: string;
+  /**
+   * Button kind
+   */
+  kind?: 'default' | 'error' | 'ghost';
   /**
    * Optional click handler
    */
-
-  onClick?: () => void;
+  onclick?: () => void;
 }
 
 export const Button3 = ({
+  borderless = false,
   kind = 'default',
+  size = 'medium',
+  label,
   ...props
 }: React.ButtonHTMLAttributes<HTMLButtonElement> & ButtonProps) => {
   return (
     <button
-      className={clsx('rounded-lg px-3 py-1 text-sm font-medium', {
-        'bg-gray-700 text-gray-100 hover:bg-gray-500 hover:text-white':
-          kind === 'default',
-        'bg-vercel-pink text-red-50 hover:bg-pink-600 hover:text-white':
-          kind === 'error',
-      })}
+      className={clsx(
+        'rounded-lg border-2 border-solid px-3 py-1 font-medium disabled:cursor-not-allowed disabled:opacity-25',
+        {
+          'bg-gray-700 text-gray-100 hover:bg-gray-500 hover:text-white':
+            kind === 'default',
+          'bg-vercel-pink text-red-50 hover:bg-pink-600 hover:text-white':
+            kind === 'error',
+          ' hover:text-white': kind === 'ghost',
+          'text-sm': size === 'small',
+          'text-md': size === 'medium',
+          'text-lg': size === 'large',
+          'border-transparent': borderless,
+          'border-slate-200': !borderless,
+        },
+      )}
       {...props}
-    />
+    >
+      {label}
+    </button>
   );
 };
